af618a13e276eb329b24c90ef46d16db1b209288
chrismalcolm <malcochris123@hotmail.co.uk>
2024-11-17 15:37:05 +0000

    main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -94,32 +94,80 @@ resource "aws_db_subnet_group" "rds_subnets" {
   subnet_ids = module.vpc.private_subnets
 }
 
+# IAM Role and Policy for EC2 to Access ECR
+resource "aws_iam_role" "ec2_role" {
+  name               = "flask-app-ec2-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "ec2_ecr_policy" {
+  name   = "flask-app-ecr-policy"
+  role   = aws_iam_role.ec2_role.name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchGetImage",
+          "ecr:GetDownloadUrlForLayer",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# Attach the IAM Role to the EC2 instance
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "flask-app-ec2-instance-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
 # EC2 Instance for Flask App
 resource "aws_instance" "flask_app" {
   ami           = "ami-0658158d7ba8fd573"
-  instance_type = "t2.micro"
-
+  instance_type = "t3.micro"
   key_name      = aws_key_pair.flask_key.key_name
   subnet_id     = module.vpc.public_subnets[0]
-  security_groups = [aws_security_group.ec2_sg.name]
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  associate_public_ip_address = true
 
   user_data = <<-EOT
               #!/bin/bash
+              # Update and install Docker
               sudo yum update -y
-              sudo yum groupinstall -y "Development Tools"
-              sudo yum install -y gcc libpq-devel python3-devel git docker
-              sudo amazon-linux-extras enable epel
-              sudo yum install -y https://repo.ius.io/ius-release-el7.rpm
-              sudo yum install -y python312 python312-pip python312-devel
-              sudo alternatives --install /usr/bin/python3 python3 /usr/bin/python3.12 1
-              sudo alternatives --config python3 <<< 1
-              sudo python3 -m ensurepip --upgrade
-              sudo pip3 install --upgrade pip
-              sudo systemctl start docker
+              sudo amazon-linux-extras enable docker
+              sudo yum install -y docker
+              sudo service docker start
               sudo usermod -a -G docker ec2-user
-              git clone https://github.com/chrismalcolm/home-monitor.git /home/ec2-user/app
-              cd /home/ec2-user/app
-              docker-compose up -d
+
+              # Install AWS CLI to authenticate Docker with ECR
+              sudo yum install -y aws-cli
+
+              # Authenticate Docker to ECR
+              aws_account_id=$(aws sts get-caller-identity --query Account --output text --region eu-north-1)
+              aws ecr get-login-password --region eu-north-1 | sudo docker login --username AWS --password-stdin $aws_account_id.dkr.ecr.eu-north-1.amazonaws.com
+
+              # Pull the image from ECR
+              docker pull $aws_account_id.dkr.ecr.eu-north-1.amazonaws.com/flask-app:latest
+
+              # Stop and remove any old container
+              docker stop flask-container || true
+              docker rm flask-container || true
+
+              # Run the container
+              docker run -d --name flask-container -p 5000:5000 $aws_account_id.dkr.ecr.eu-north-1.amazonaws.com/flask-app:latest
             EOT
 }
 
